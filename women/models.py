b8dd6ac4a0ae48689996779931bc71c670a874e7
sautenko <sautenkoandrey@gmail.com>
2022-12-14 11:55:42 +0200

    started new django-project for DRF practice. Added 2 model Women and Category. Change in settings.py language, time and registered my app women.

---
+++ women/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Women(models.Model):
+    '''Моедль статьи с заголовком, контентом, временем создания,
+    временем изменения(возможным), функцией опубликовано/неопубликовано
+    и ссылкой на категории (ссылкой на модель Category)'''
+    title=models.CharField(max_length=255)
+    content=models.TextField(blank=True)
+    time_create=models.DateTimeField(auto_now_add=True)
+    time_update=models.DateTimeField(auto_now=True)
+    is_published=models.BooleanField(default=True)
+    cat=models.ForeignKey('Category',on_delete=models.PROTECT,null=True)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    '''Модель определяет что это за категория знаменитых женщин:
+    актриса, певица или спортсменка'''
+    name=models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
